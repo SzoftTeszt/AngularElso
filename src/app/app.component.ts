e7cb079db6fa216a377a2d1a02b80559dbf44dd0
Jáger Attila <jagerattila@gmail.com>
2024-03-04 18:47:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ElsoAngular';
+  sajat  ="Nyuszika"
+  tomb =["Attila", "András", "Laura", "Zsuzsi"]
+
+  newFruit:any={}
+
+  oszlopok:any=
+  [
+    {key:"id", text:"#", type:"plain"},
+    {key:"name", text:"Név", type:"text"},
+    {key:"price", text:"Ár", type:"number"},
+  ]
+
+  adat:any= [
+    {id:1, name:"Körte", price:2000},
+    {id:2, name:"Alma", price:1500},
+    {id:3, name:"Szilva", price:3000},
+  ]
+
+  torles(gyumi:any){
+      this.adat=this.adat.filter(
+        (gyumolcs:any)=> gyumolcs != gyumi 
+      )
+  }
+
+  hozzaadas(){
+    this.newFruit.id=10
+    this.adat.push(this.newFruit)
+    this.newFruit={}
+  }
 }
